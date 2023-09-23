7b8535a1973668abd75fd932df68dbd1b306025d
AnkitSoni2001 <ankitsoni221010@gmail.com>
2023-09-23 14:07:38 +0530

    fifth version

--- QuizApp/backend/index.js
+++ QuizApp/backend/index.js
@@ -1,5 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express')
+var authRouter = require('./routes/auth')
+var quizRouter = require('./routes/quiz')
 var cors = require('cors')
 const app = express()
 const port = 1000
@@ -8,6 +10,9 @@ app.use(cors())
 app.use(express.json())//if we want to use req.body this middleware is required
 
 // Available Routes
+app.use('/api/auth', authRouter)
+app.use('/api/quiz', quizRouter)
+
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/quiz', require('./routes/quiz'))
 
